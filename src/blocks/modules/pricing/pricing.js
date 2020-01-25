ac57666de7927d5fb5b1f6020c532493601f1958
Viktor Kogai <vandal.vector@outlook.com>
2020-01-25 22:55:53 +1000

    add struct reviews

---
+++ src/blocks/modules/pricing/pricing.js
@@ -0,0 +1,13 @@
+modules.define('pricing', ['i-bem-dom'], function(provide, bemDom) {
+
+provide(bemDom.declBlock(this.name, {
+    onSetMod: {
+        js: {
+            inited: function() {
+                
+            }
+        }
+    }
+}));
+
+});
